af6fed6cd88d6b989fa9e9b6a8d6917562d09ace
Ravele Silva <ravele.cs@gmail.com>
2023-04-18 01:02:54 -0300

    Efeito de sobreposição no menu mobile

--- JavaScript/menu-mobile.js
+++ JavaScript/menu-mobile.js
@@ -1,9 +1,13 @@
 const menuHamburger = document.querySelector('.menu-hamburger')
 const headerNav = document.querySelector('.header-nav')
+const backshadow = document.querySelector('#backshadow')
+
+console.log(backshadow)
 
 function toggleMenu(){
     menuHamburger.classList.toggle('ativo')
     headerNav.classList.toggle('ativo')
+    backshadow.classList.toggle('ativo')
 }
 
 menuHamburger.addEventListener('click', toggleMenu)
